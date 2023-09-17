cc70c5c4d3c455d0ce1104bd5e7dc8b651cda9ec
yoonpunk <yoonys427@outlook.com>
2023-09-17 19:41:14 +0900

    HelloJob 생성

--- src/main/java/com/example/springbatch/SpringBatchApplication.java
+++ src/main/java/com/example/springbatch/SpringBatchApplication.java
@@ -3,6 +3,8 @@ package com.example.springbatch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableBatchProcessing
