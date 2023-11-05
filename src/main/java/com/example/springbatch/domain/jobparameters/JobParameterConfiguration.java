d12707060b45ddbbd9327be00a027b48712ca90a
yoonpunk <yoonys427@outlook.com>
2023-11-06 00:38:32 +0900

    3장 JobParameter 실습

--- src/main/java/com/example/springbatch/domain/jobparameters/JobParameterConfiguration.java
+++ src/main/java/com/example/springbatch/domain/jobparameters/JobParameterConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.springbatch;
+package com.example.springbatch.domain.jobparameters;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
--- src/main/java/com/example/springbatch/domain/jobparameters/JobParameterTest.java
+++ src/main/java/com/example/springbatch/domain/jobparameters/JobParameterTest.java
@@ -1,4 +1,4 @@
-package com.example.springbatch;
+package com.example.springbatch.domain.jobparameters;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
@@ -11,15 +11,20 @@ import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
-//@Component
+@Component
 public class JobParameterTest implements ApplicationRunner {
 
-//    @Autowired
+    @Autowired
     private JobLauncher jobLauncher;
 
-//    @Autowired
+    @Autowired
     private Job job;
 
+    /**
+     * command line에서 jar로 실행 시 아래와 같은 파라미터를 넣으려면 다음과 같은 명령어로 실행한다.
+     * java -jar spring-batch-0.0.1-SNAPSHOT.jar 'name=user1' 'seq(long)=2L' 'date(date)=2023/11/6' 'age(double)=16.5'
+     * 주의사항: 파라미터 전달 시 따옴표를 사용할 것
+     */
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
